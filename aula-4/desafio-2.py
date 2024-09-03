5a0c6b4fb699d371872e3395f683418908d255e8
Hudson Santos <hudsnss@gmail.com>
2024-09-02 21:09:43 -0300

    feat: aula 4

---
+++ aula-4/desafio-2.py
@@ -0,0 +1,5 @@
+dia = input('dia = ')
+mes = input('mes = ')
+ano = input('ano = ')
+
+print('Vc nasceu dia', dia, 'do mês', mes, 'do ano', ano, '. Correto?')
---
+++ aula-4/desafio-3.py
@@ -0,0 +1,7 @@
+primeiro_numero = input('primeiro número ')
+segundo_numero = input('segundo número ')
+
+int_primeiro_numero = int(primeiro_numero)
+int_segundo_numero = int(segundo_numero)
+
+print(int_primeiro_numero + int_segundo_numero)
